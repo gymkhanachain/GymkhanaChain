48c12da9f8c57669b37324fbc5faf16573397ad1
Ale <aletibazo@gmail.com>
2019-03-15 19:47:28 +0100

    Añadí soporte para fragmentos en MainActivity y el MapFragment, que se carga cuando se inicia la actividad.
    También añadí la paleta de colores.

--- App/app/src/main/java/com/gymkhanachain/app/MainActivity.java
+++ App/app/src/main/java/com/gymkhanachain/app/MainActivity.java
@@ -1,13 +1,30 @@
 package com.gymkhanachain.app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MapFragment.OnMapFragmentInteractionListener {
+
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        MapFragment mapFragment = new MapFragment();
+        fragmentTransaction.add(R.id.placeholder_main, mapFragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onMapFragmentInteraction(Uri uri) {
+
     }
 }
