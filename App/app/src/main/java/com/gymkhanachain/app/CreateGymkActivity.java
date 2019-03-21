ae186434e2495629edf861ef80ccb657744ab1b5
Ale <aletibazo@gmail.com>
2019-03-22 00:45:10 +0100

    Interfaz "mock" de crear gymkhana (detalles y puntos) implementada.
    La pestaña "Puntos" carga el fragmento MapFragment.
    
    Quité el menú superior, ya que en esta actividad no es necesario.

--- App/app/src/main/java/com/gymkhanachain/app/CreateGymkActivity.java
+++ App/app/src/main/java/com/gymkhanachain/app/CreateGymkActivity.java
@@ -9,13 +9,12 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class CreateGymkActivity extends AppCompatActivity implements GymkDetailsFragment.OnFragmentInteractionListener, GymkPointsFragment.OnFragmentInteractionListener, GymkPrivacyFragment.OnFragmentInteractionListener {
+public class CreateGymkActivity extends AppCompatActivity implements GymkDetailsFragment.OnFragmentInteractionListener,
+        GymkPointsFragment.OnFragmentInteractionListener, GymkPrivacyFragment.OnFragmentInteractionListener, MapFragment.OnMapFragmentInteractionListener {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -54,31 +53,13 @@ public class CreateGymkActivity extends AppCompatActivity implements GymkDetails
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_create_gymk, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @Override
     public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onMapFragmentInteraction(Uri uri) {
 
     }
 
@@ -86,16 +67,19 @@ public class CreateGymkActivity extends AppCompatActivity implements GymkDetails
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
+    /* TODO Deberíamos instanciar estos fragmentos utilizando el newInstance() pasándole parámetros
+     *  Así podemos instanciar MapFragment desde donde queramos utilizando distintas funciones en cada caso.
+        */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         List<String> fragments;
 
-        public SectionsPagerAdapter(FragmentManager fm) {
+        private SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
 
             fragments = new ArrayList<>();
             fragments.add(GymkDetailsFragment.class.getName());
-            fragments.add(GymkPointsFragment.class.getName());
+            fragments.add(MapFragment.class.getName());
             fragments.add(GymkPrivacyFragment.class.getName());
         }
 
