c40ca66bf84e4efe1b297b7ee7aeaf60c45ae261
losete <losetazo@gmail.com>
2019-05-15 18:16:25 +0200

    Añadido endpoint en cloud, aplicación desplegada en Google Cloud

--- App/app/src/main/java/com/gymkhanachain/app/client/ApiRoutes.java
+++ App/app/src/main/java/com/gymkhanachain/app/client/ApiRoutes.java
@@ -3,7 +3,7 @@ package com.gymkhanachain.app.client;
 public class ApiRoutes {
 
 
-    public static final String ROOT_URL="http://10.0.2.2:5002";
+    public static final String ROOT_URL="http://35.211.136.52";
     public static final String GYMKHANAS="/gymkhanas";
 
 }
